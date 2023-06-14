a6b1c57edc678257912738364fae3143ce5d1e10
song-inhyo <izp1012@naver.com>
2023-06-15 00:10:05 +0900

    4.6 조회 완성... 1. 무슨 이상한 Error.html 뷰 리졸버 오류 발생하였는데, 아마 build.gradle 오류였던것으로 보ㅇ임 해결하엿음 2. 그리고 화면 보여주는 단에서 redirect: 부분에 '/' 가 포함되어있는 경우 인식오류? 발생해서 페이지 전환안됨 일단 제거해둔 상태 3. 그리고 PageRequestDTO 에서 뭐가 문젠지 모르겟으나 한수님 파일로 업데이트하니 목록 조회 성공하였음

--- src/main/java/com/study/guestbook/controller/GuestbookController.java
+++ src/main/java/com/study/guestbook/controller/GuestbookController.java
@@ -1,19 +1,50 @@
 package com.study.guestbook.controller;
 
+import com.study.guestbook.dto.PageRequestDTO;
+import com.study.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor //자동 주입을 위한 Annotation
 public class GuestbookController {
-    @GetMapping({"/","/list"})
-    public String list(){
 
-        log.info("list..........");
+    private final GuestbookService service;
 
-        return "/guestbook/list";
+    @GetMapping("/")
+    public String index(){
+
+        return "redirect:guestbook/list";
+    }
+
+    @GetMapping({"/list"})
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        /*파라미터로 pageRequestDTO 를 사용하였는데,
+        스프링 MVC 는 파라미터를 자동으로 수집해 주는 기능이 있기때문에,
+        화면에서 page와 size 라는 파라미터를 전달하면 pageRequestDTO 객체로 자동으로 수집될 것이다.
+         */
+
+        log.info("list.........." + pageRequestDTO);
+        System.out.println(pageRequestDTO);
+        System.out.println(service.getList(pageRequestDTO));
+        model.addAttribute("result", service.getList(pageRequestDTO));
+
+//        return "/guestbook/list";
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @GetMapping("/error")
+    public void error(){
+        log.info("error...");
     }
 }
--- src/main/java/com/study/guestbook/dto/PageRequestDTO.java
+++ src/main/java/com/study/guestbook/dto/PageRequestDTO.java
@@ -1,5 +1,6 @@
 package com.study.guestbook.dto;
 
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,26 +8,28 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
-
 @Builder
 @AllArgsConstructor
 @Data
-/*
+
+    /*
     이 클래스의 목적 : JPA 쪽에서 사용하는 Pageable 타입의 객체를 생성하는 것
     추후 수정의 여지 존재 (페이지 번호에 음수가 들어오는 경우 )
     그러나 JPA 이용하는 경우 페이지 번호가 0부터 시작한다는 점을 감안하여 1페이지의 경우 0이 되도록 page-1 하는 형태로 작성
- */
-
+    */
 public class PageRequestDTO {
     private int page;
     private int size;
 
-    public void PageRequestDTO(){
+    public PageRequestDTO(){
         this.page = 1;
         this.size = 10;
     }
 
     public Pageable getPageable(Sort sort){
+        //페이지번호가 1이 들어올 경우 0이 될 수 있도록 -1을 해줌
         return PageRequest.of(page -1, size, sort);
+
     }
+
 }
