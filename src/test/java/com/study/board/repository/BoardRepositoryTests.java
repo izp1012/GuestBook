6e8b6c72ee371ed7044b0af0a8a4740d9c999665
song-inhyo <izp1012@naver.com>
2023-06-28 00:15:58 +0900

    - 목록화면, 조회화면 에서 사용될 JPQL

--- src/test/java/com/study/board/repository/BoardRepositoryTests.java
+++ src/test/java/com/study/board/repository/BoardRepositoryTests.java
@@ -6,6 +6,10 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Arrays;
 import java.util.List;
@@ -59,5 +63,30 @@ public class BoardRepositoryTests {
         for(Object[] arr : result){
             System.out.println(Arrays.toString(arr));
         }
+    }
+
+    @Test
+    public void testWithReplyCount(){
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.getBoardWithReplyCount(pageable);
+
+        result.get().forEach(row -> {
+
+            Object[] arr = (Object[])row;
+
+            System.out.println(Arrays.toString(arr));
+        });
+    }
+
+    @Test
+    public void testRead3() {
+
+        Object result = boardRepository.getBoardByBno(100L);
+
+        Object[] arr = (Object[])result;
+
+        System.out.println(Arrays.toString(arr));
     }
 }
