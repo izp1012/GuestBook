4bc97b563f63195d9ca97a043b74ef56b245290e
song-inhyo <izp1012@naver.com>
2023-06-28 01:05:09 +0900

    - 5.4.1 게시글 조회 처리 (화면 및 Controller 연결)

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,27 @@
+package com.study.board.controller;
+
+import com.study.board.dto.PageRequestDTO;
+import com.study.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list............." + pageRequestDTO);
+
+        model.addAttribute("result", boardService.getList(pageRequestDTO));
+
+    }
+}
