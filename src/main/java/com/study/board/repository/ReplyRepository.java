1f8789339809cdaaa044df015b026dc45aa74956
song-inhyo <izp1012@naver.com>
2023-06-29 00:54:32 +0900

    - 6.1 JSON 과 Ajax 로 댓글처리 Repository 수정 및 테스트

--- src/main/java/com/study/board/repository/ReplyRepository.java
+++ src/main/java/com/study/board/repository/ReplyRepository.java
@@ -1,13 +1,20 @@
 package com.study.board.repository;
 
+import com.study.board.entity.Board;
 import com.study.board.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long>{
 
+    //Board 삭제시에 댓글들 삭제
     @Modifying
     @Query("delete from Reply r where r.board.bno =:bno ")
     void deleteByBno(Long bno);
+
+    //게시물로 댓글 목록 가져오기
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
