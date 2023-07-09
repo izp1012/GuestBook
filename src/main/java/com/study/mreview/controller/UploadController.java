133fe89da87a93dc69465283505d453b18946558
song-inhyo <izp1012@naver.com>
2023-07-09 23:59:14 +0900

    - 8.1.3 업로드 결과 반환과 화면 처리
    <브라우저에 필요한 정보>
    - 업로드된 파일의 원래 이름
    - 파일의 UUID 값
    - 업로드된 파일의 저장 경로

--- src/main/java/com/study/mreview/controller/UploadController.java
+++ src/main/java/com/study/mreview/controller/UploadController.java
@@ -1,19 +1,27 @@
 package com.study.mreview.controller;
 
+import com.study.mreview.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,15 +30,50 @@ public class UploadController {
     @Value("${org.inhyo.upload.path}")  //applicaion.properties 의 변수
     private String uploadPath;
 
+    /*
+    URL 인코딩된 파일 이름을 파라미터로 받아서 해당 파일을 byte[] 로 만들어서 브라우저로 전송
+    파일의 확장자에 따라서 브라우저에 전송하는 MIME 타입이 달라져야 하는 문제는 java.nio.file 패키지의 File.probeContentType() 을 이용해서 처리
+    파일 데이터의 처리는 스프링에서 제공하는 org.springframework.util.FileCopyUtils 를 이용해서 처리
+    브라우저의 업로드된 결과 중에 imageURL 속성이 있다.(UploadResultDTO 의 getImageURL())
+     */
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result = null;
+
+        try {
+            String srcFileName =  URLDecoder.decode(fileName,"UTF-8");
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath +File.separator+ srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            //MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+            //파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
+    }
+
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
 
         for(MultipartFile uploadFile : uploadFiles){
 
             //이미지 파일만 업로드 가능
             if(uploadFile.getContentType().startsWith("image") == false) {
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 
             //실제 파일 이름 IE 나 Edg량e 는 전체 경로가 들어오므로
@@ -51,10 +94,12 @@ public class UploadController {
 
             try{
                 uploadFile.transferTo(savePath);
+                resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
             }catch (IOException e){
                 e.printStackTrace();
             }
         }//end for
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
 
     private String makeFolder() {
